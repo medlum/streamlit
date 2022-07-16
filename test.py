c0f639e02427796a28d950b91100efd9c8de1544
andy oh <andy_oh18@yahoo.com>
2022-07-16 12:40:22 +0800

    test

--- test.py
+++ test.py
@@ -1,10 +1,16 @@
-from secrets import choice
-from unittest import result
+import folium
 import streamlit as st
+from streamlit_folium import folium_static
 import numpy as np
-import pandas as pd
+import pandas
+st.set_page_config(layout="wide")
+
+m = folium.Map(location=[23.47, 77.94], tiles="CartoDB positron", name = "Light Map", zoom_start=5, attr="My Data")
+
+folium_static(m, width=950, height=560)
+
 # title
-st.title("Hello World!")
+#st.title("Hello World!")
 #st.header("This is a header")
 #st.subheader("This is a subheader")
 #st.text("Hello Andy!")
@@ -15,7 +21,7 @@ st.title("Hello World!")
 #                         ["Michael Jackson", "Elvis Presley",
 #                          "Eminem", "Billy Joel", "Madonna"])
 
-actions = {"A": st.write, "B": st.write, "C": st.write}
-choice = st.selectbox("Choose one:", ["_", "A", "B", "C"])
-if choice != "_":
-    result = actions[choice](f"You chose {choice}")
+#actions = {"A": st.write, "B": st.write, "C": st.write}
+#choice = st.selectbox("Choose one:", ["_", "A", "B", "C"])
+#if choice != "_":
+#    result = actions[choice](f"You chose {choice}")
